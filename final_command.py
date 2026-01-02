7d798c7fd32b5807865172968812a86e14bf14ee
ruanjinchen <ruanjinchen@foxmail.com>
2026-01-02 14:26:34 +0800

    更新训练指令参数

--- final_command.py
+++ final_command.py
@@ -255,77 +255,6 @@ python train_flowmatching.py \
   --sample_steps 100 \
   --out_dir runs_final/sim_5m_with_base_hybrid_12_28
 
-export CUDA_VISIBLE_DEVICES=0,1
-torchrun --standalone --nproc_per_node=2 --master_port=29511 \
-  train_flowmatching.py \
-  --data_dir datasets/sim/5m_with_base \
-  --batch_size 4 --epochs 500 --save_every 20 \
-  --tr_max_sample_points 20000 --te_max_sample_points 20000 \
-  --cond_mode motors \
-  --pf_backbone hybrid \
-  --emb_dim 64 --width 256 --depth 4 --cfg_drop_p 0.0 \
-  --ctx_dim 16 \
-  --ctx_emb_dim 64 \
-  --ctx_stage_channels 80 112 112 \
-  --ctx_stage_blocks 2 2 2 \
-  --ctx_stage_res 48 32 16 \
-  --ctx_with_se --ctx_with_global --ctx_voxel_normalize \
-  --ctx_t_gate_tau 0.97 --ctx_t_gate_k 12 \
-  --use_cosine_lr \
-  --use_rgb --rgb_key rgb \
-  --lambda_color 0.05 \
-  --t_beta_a 3.0 \
-  --point_prior_std 0.5 \
-  --sample_steps 100 --train_fraction 0.625 \
-  --out_dir runs_final/sim_5m_with_base_hybrid_12_30_new_hybrid_params_half_data
-
-export CUDA_VISIBLE_DEVICES=2,3
-torchrun --standalone --nproc_per_node=2 --master_port=29511 \
-  train_flowmatching.py \
-  --data_dir sim/5m_with_base \
-  --batch_size 4 --epochs 500 --save_every 20 \
-  --tr_max_sample_points 20000 --te_max_sample_points 20000 \
-  --cond_mode motors \
-  --pf_backbone hybrid \
-  --emb_dim 64 --width 256 --depth 4 --cfg_drop_p 0.0 \
-  --ctx_dim 16 \
-  --ctx_emb_dim 64 \
-  --ctx_stage_channels 80 112 112 \
-  --ctx_stage_blocks 2 2 2 \
-  --ctx_stage_res 48 32 16 \
-  --ctx_with_se --ctx_with_global --ctx_voxel_normalize \
-  --ctx_t_gate_tau 0.97 --ctx_t_gate_k 12 \
-  --use_cosine_lr \
-  --use_rgb --rgb_key rgb \
-  --lambda_color 0.05 \
-  --t_beta_a 3.0 \
-  --point_prior_std 0.5 \
-  --sample_steps 100 \
-  --out_dir runs_final/sim_5m_with_base_hybrid_12_30_new_hybrid_params_new_data
-
-export CUDA_VISIBLE_DEVICES=4,5
-torchrun --standalone --nproc_per_node=2 --master_port=29511 \
-  train_flowmatching.py \
-  --data_dir datasets/sim/5m_with_base \
-  --batch_size 4 --epochs 500 --save_every 20 \
-  --tr_max_sample_points 20000 --te_max_sample_points 20000 \
-  --cond_mode motors \
-  --pf_backbone hybrid \
-  --emb_dim 256 --width 512 --depth 6 --cfg_drop_p 0.0 \
-  --ctx_dim 64 \
-  --ctx_emb_dim 256 \
-  --ctx_stage_channels 80 112 112 \
-  --ctx_stage_blocks 2 2 2 \
-  --ctx_stage_res 24 16 8 \
-  --ctx_with_se --ctx_with_global --ctx_voxel_normalize \
-  --ctx_t_gate_tau 0.80 --ctx_t_gate_k 12 \
-  --use_cosine_lr \
-  --use_rgb --rgb_key rgb \
-  --lambda_color 0.05 \
-  --t_beta_a 3.0 \
-  --point_prior_std 0.5 \
-  --sample_steps 100 \
-  --out_dir runs_final/sim_5m_with_base_hybrid_bighead_tau0.8
 
 export CUDA_VISIBLE_DEVICES=1
 python train_flowmatching.py \
@@ -399,18 +328,17 @@ python train_flowmatching.py \
   --out_dir runs_final/real_2m_with_base_mlp_12_29
 
 real 2节 有base的，10000组数据 hybrid版本 有RGB H100：
-新参数，匹配了MLP的width depth emb_dim, 训练进行中
-export CUDA_VISIBLE_DEVICES=0,1
-torchrun --standalone --nproc_per_node=2 --master_port=29511 \
-  train_flowmatching.py \
+重跑中，最新的Hybrid的参数, 训练进行中
+export CUDA_VISIBLE_DEVICES=5
+python train_flowmatching.py \
   --data_dir datasets/real/2m_with_base \
-  --batch_size 4 --epochs 500 --save_every 20 \
+  --batch_size 32 --lr 8e-4 --warmup_steps 4000 --epochs 500 --save_every 20 \
   --tr_max_sample_points 20000 --te_max_sample_points 20000 \
   --cond_mode motors \
   --pf_backbone hybrid \
   --emb_dim 256 --width 512 --depth 6 --cfg_drop_p 0.0 \
-  --ctx_dim 16 \
-  --ctx_emb_dim 64 \
+  --ctx_dim 64 \
+  --ctx_emb_dim 256 \
   --ctx_stage_channels 80 112 112 \
   --ctx_stage_blocks 2 2 2 \
   --ctx_stage_res 24 16 8 \
@@ -421,8 +349,8 @@ torchrun --standalone --nproc_per_node=2 --master_port=29511 \
   --lambda_color 0.05 \
   --t_beta_a 3.0 \
   --point_prior_std 0.5 \
-  --sample_steps 100 \
-  --out_dir runs_final/real_2m_with_base_hybrid_1_1
+  --sample_steps 50 \
+  --out_dir runs_final/real_2m_with_base_hybrid_1_2
 
 -------------------------------------------------------------------------------------------------------------------------
 
@@ -445,18 +373,17 @@ python train_flowmatching.py \
   --out_dir runs_final/real_3m_with_base_mlp_12_30
 
 real 2节 有base的，10000组数据 hybrid版本 有RGB H100：
-√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√√
-epoch 0500] CD-L2(cond) mean = 0.000098
-export CUDA_VISIBLE_DEVICES=1
+重跑中，更新了Hybrid的参数
+export CUDA_VISIBLE_DEVICES=4
 python train_flowmatching.py \
   --data_dir datasets/real/3m_with_base \
-  --batch_size 8 --epochs 500 --save_every 20 \
+  --batch_size 32 --lr 8e-4 --warmup_steps 4000 --epochs 500 --save_every 20 \
   --tr_max_sample_points 20000 --te_max_sample_points 20000 \
   --cond_mode motors \
   --pf_backbone hybrid \
-  --emb_dim 64 --width 256 --depth 4 --cfg_drop_p 0.0 \
-  --ctx_dim 16 \
-  --ctx_emb_dim 64 \
+  --emb_dim 256 --width 512 --depth 6 --cfg_drop_p 0.0 \
+  --ctx_dim 64 \
+  --ctx_emb_dim 256 \
   --ctx_stage_channels 80 112 112 \
   --ctx_stage_blocks 2 2 2 \
   --ctx_stage_res 24 16 8 \
@@ -467,6 +394,7 @@ python train_flowmatching.py \
   --lambda_color 0.05 \
   --t_beta_a 3.0 \
   --point_prior_std 0.5 \
-  --sample_steps 100 \
-  --out_dir runs_final/real_3m_with_base_hybrid_12_30
+  --sample_steps 50 \
+  --out_dir runs_final/real_3m_with_base_hybrid_1_2
+
 '''
